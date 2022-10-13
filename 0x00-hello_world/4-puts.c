336c867b2ade4ef7940033f4b1153badbf3789aa
Neo Mphelane <mphelanen10671@gmail.com>
2022-10-13 16:57:20 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/*
+* main is entry
+* Return always 0
+*/
 
 int main(void)
 {
-	Write(""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
