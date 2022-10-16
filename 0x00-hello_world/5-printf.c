b8edd278483bfdc0c4a0a43bc4ebe72f50cba812
Neo Mphelane <mphelanen10671@gmail.com>
2022-10-16 16:31:26 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-\**
-  * main - Prints a text
-  * 
-  * Return - Always 0 (Successful)
-*/
+
+/**
+ * main - Entry point
+ * Description: use printf
+ * Return - Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
