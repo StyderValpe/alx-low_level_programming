0939786623d0db50db6b4ae31420f0d9479ed7bc
Neo Mphelane <mphelanen10671@gmail.com>
2022-10-17 00:55:58 -0700

    Task 6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,12 @@
  * main - entry point
  * Description: single digits of base 10 starting from 0 using putchar
  * Return: 0
- *    */
+ */
 
 int main(void)
 {
 	int s;
+
 	for (s = 0; s < 10; s++)
 	{
 		putchar(s + '0');
