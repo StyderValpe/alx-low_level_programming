f091435952295c6701fd6bc6b92477c963d1ceaf
Neo Mphelane <mphelanen10671@gmail.com>
2022-10-17 00:49:44 -0700

    Task 9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,14 +5,15 @@
  * main - entry point
  * Description: prints all possible combinations of single-digit numbers
  * Return: 0
- */
+*/
 int main(void)
 {
 	int n;
 
-	for (n = '0'; n < = '9'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
+
 		if (n != '9')
 		{
 			putchar(',');
