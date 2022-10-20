5028cd8b22a470c81beac2d8ec76eead35050b34
Neo Mphelane <mphelanen10671@gmail.com>
2022-10-20 14:40:47 -0700

    Task 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - entry point
+ * a - check character
+ * b - check character
+ * Description: multiply two intergers
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
