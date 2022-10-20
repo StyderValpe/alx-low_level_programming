bd742e278b244436a6997bb23769eff8e002de6d
Neo Mphelane <mphelanen10671@gmail.com>
2022-10-19 19:17:03 -0700

    Task 3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - entry point
+ * Description: check for lowercase characters
+ * Return 1 if lowercase, 0 if uppercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
