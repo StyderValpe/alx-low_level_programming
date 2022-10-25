5c07451014db1a2a970506365ee2d0f91a9da128
Neo Mphelane <mphelanen10671@gmail.com>
2022-10-25 14:47:07 -0700

    Task 2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ * c: character check
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+		while (*(c + i) != '\0')
+			i++;
+	return (i);
+}
